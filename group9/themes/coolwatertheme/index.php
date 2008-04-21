ac0730a7e8fdc5995981a94ed20c209c308c2c1d
immanuel192 <immanuel192@f9e4ffa1-c34a-0410-8d1e-3168130c43b5>
2008-04-21 18:16:50 +0000

    Trung update template ngày 22/04/2008 01:15AM

--- group9/themes/coolwatertheme/index.php
+++ group9/themes/coolwatertheme/index.php
@@ -1,59 +1,81 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<?php 
 	$config = array(
-	"reset-fonts-grids"=>false,
+	"reset-fonts-grids"=>true,
 	"include-common-css"=>true,
 	"include-theme-css"=>true
 	);
 	echo exponent_theme_headerInfo($section,$config); 
 	?>
+	
 </head>
+
 <body>
 <?php exponent_theme_sourceSelectorInfo(); //this will be deprecated by copy 'n paste in beta ?>
-<!-- wrap starts here -->
-<div id="wrap">
-	<!--header -->
-	<div id="header">			
-		<h1 id="logo-text"><a href="<?php echo URL_FULL; ?>index.php">ex<span class="green">ponent</span> <sup>CMS</sup></a></h1>		
-		<p id="slogan">The "coolwater" theme from Styleshout.com</p>		
-		<div id="header-links">
-			<a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">Home</a> | 
-			<a href="<?php echo exponent_core_makeLink(array('section'=>16)); ?>">Contact Us</a> | 
-			<a href="<?php echo exponent_core_makeLink(array('section'=>10)); ?>">Site-map</a>
+<div id="outer">
+	<div id="wrapper">
+		<div id="nav">
+		  <div id="nav-left">
+		    <div id="nav-right">
+    			<ul>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">ABOUT US</a></li>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">PRODUCTS</a></li>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">SERVICES</a></li>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">SHOPPING CART</a></li>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">NEW GADGETS</a></li>
+    			  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">REGISTER</a></li>
+    			</ul>
+		    </div>
+		  </div>
+			<div class="clear"></div>
 		</div>
-		<div id="header-login">
-			<?php exponent_theme_showModule("loginmodule","Expanded"); ?>
+		<div id="head">
+			<div id="head-left"></div>
+			<div id="head-right"></div>
+			<div id="head-1"></div>
+			<h1><span class="logo"><span class="top">top</span><span class="gadgets">gadgets</span></span></h1>
+			<div id="navb">
+			  <ul>
+				  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">HOME</a></li>
+				  <li><a href="<?php echo exponent_core_makeLink(array('section'=>SITE_DEFAULT_SECTION)); ?>">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="head-2"></div>
+		<div id="login">
+			<div id="login-bot">
+				<div id="login-box">
+					<?php exponent_theme_showModule("loginmodule","Expanded"); ?>
+				</div>
+				<div id="login-welcome">
+					<div>
+						<h2>Welcome</h2>
+						<p>Don't forget to check <a href="<?php echo URL_FULL; ?>">Liberty Electric</a> every day, because we add  new products almost daily.</p>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<div id="body">
+			<div id="body-bot">
+					<h2></h2><?php exponent_theme_showModule("navigationmodule","Breadcrumb","","@top"); ?></h2>
+					<div id="items">
+						<?php exponent_theme_main(); ?>
+						<div class="clear"></div>
+					</div>
+					<?php exponent_theme_showModule("textmodule","Default","","barner"); ?>
+					<div id="footer">
+						<div id="footloose"><span class="logo"><span class="top">top</span><span class="gadgets">gadgets</span></span></div>
+						<p><a href="http://www.freewebsitetemplates.com">Privacy Policy</a> <strong>&nbsp;:&nbsp;</strong> <a href="http://www.freewebsitetemplates.com">Terms &amp; Conditions</a> <br />
+						&copy; Copyright 2007. All rights reserved.</p>
+					</div>					
+			</div>
 		</div>
 	</div>
-	<!-- navigation -->
-	<div  id="menu">
-		<?php exponent_theme_showModule("navigationmodule","YUI Top Nav","","@top"); ?>
-	</div>
-	<!-- content-wrap starts here -->
-	<div id="content-wrap">
-		
-		<div id="main">
-			<?php exponent_theme_main(); ?>
-		</div>
-		<div id="sidebar">
-			<?php exponent_theme_showModule("containermodule","Default","","@left"); ?>			
-			<h2>Support Styleshout</h2>
-			<p>If you are interested in supporting my work and would like to contribute, you are
-			welcome to make a small donation through the 
-			<a href="http://www.styleshout.com/">donate link</a> on my website - it will 
-			be a great help and will surely be appreciated.</p>
-		</div>
-	<!-- content-wrap ends here -->	
-	</div>
-	<!--footer starts here-->
-	<div id="footer">
-		<?php exponent_theme_showModule("containermodule","Default","","@footer"); ?>				
-	</div>	
-
-
 </div>
 
 </body>
