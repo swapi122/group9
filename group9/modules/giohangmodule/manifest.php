6a2aa35f9a60a1c9c09afb0e56798ba827dec6c5
immanuel192 <immanuel192@f9e4ffa1-c34a-0410-8d1e-3168130c43b5>
2008-05-07 16:41:29 +0000

    sửa module giỏ hàng

---
+++ group9/modules/giohangmodule/manifest.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'modules/giohangmodule/class.php'=>'',
+	'modules/giohangmodule/manifest.php'=>1,
+	'modules/giohangmodule/actions/edit.php'=>'',
+	'modules/giohangmodule/actions/save.php'=>'',
+	'modules/giohangmodule/actions/delete.php'=>'',
+	'modules/giohangmodule/views/Default.tpl'=>'',
+	'modules/giohangmodule/views/_form_edit.tpl'=>'',
+	'datatypes/definitions/giohangmodule_config.php'=>'',
+	'datatypes/giohangmodule_config.php'=>'',
+
+);
+
+?>
